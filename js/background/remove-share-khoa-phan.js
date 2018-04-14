5713a1a71b472035498b0c5f4530faf676f765f6
Socola <tokentien@gmail.com>
2018-04-14 20:57:12 +0700

    update code

--- js/background/remove-share-khoa-phan.js
+++ js/background/remove-share-khoa-phan.js
@@ -1,9 +1,17 @@
+/*
+ * @Author: Socola
+ * @Date:   2018-02-01 19:58:10
+ * @Last Modified by:   Socola
+ * @Last Modified time: 2018-04-12 21:33:03
+ */
 'use strict';
 window.onload = function() {
-   tool.addScript(`
+	let scriptTag = document.createElement("script");
+	scriptTag.innerHTML = `
 	    if(document.getElementById("videoYoutubeIMG")){
 	        $.cookie('dataShare_'+post_id,true,{ expires: 3, path: '/' });
 	        location.reload();
 	    }
-	`);
+	`;
+	document.head.appendChild(scriptTag);
 };
--- js/code.js
+++
@@ -1,6 +0,0 @@
-/*
- * @Author: Socola
- * @Date:   2018-02-01 19:58:09
- * @Last Modified by:   Socola
- * @Last Modified time: 2018-03-27 13:07:02
- */
